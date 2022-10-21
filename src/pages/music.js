d7c28c104df17a4639db1bee9d9c94f3e4424d64
bdavs3 <bdavs3000@gmail.com>
2022-10-21 15:11:17 -0600

    Create the bones for the music page

---
+++ src/pages/music.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Head from "../components/head";
+import Layout from "../components/layout";
+
+const MusicPage = () => (
+  <Layout>
+    <Head title="Music" />
+    <div>Looks like you found the music page</div>
+  </Layout>
+);
+
+export default MusicPage;
